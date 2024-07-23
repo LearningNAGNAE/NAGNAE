ad048f919dacfb9c239d9a2752934e128e7bd512
yys <lukdown10@gmail.com>
2024-07-23 19:40:39 +0900

    yys

--- src/components/chatbot/Sidebar.js
+++ src/components/chatbot/Sidebar.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator } from '@chakra-ui/react'
 
 function Sidebar() {
   return (
     <aside>
       <h2>Category</h2>
-      <ul>
-        <li>Liên quan đến Chương trình cấp phép việc làm</li>
-        <li>Liên quan đến Chương trình cấp phép việc làm</li>
-        <li>Liên quan đến Chương trình cấp phép việc làm</li>
-      </ul>
+      <Tabs position='relative' variant='unstyled'>
+        <TabList>
+            <Tab>One</Tab>
+            <Tab>Two</Tab>
+            <Tab>Three</Tab>
+        </TabList>
+        <TabIndicator mt='-1.5px' height='2px' bg='blue.500' borderRadius='1px' />
+        <TabPanels>
+            <TabPanel>
+                <p>one!</p>
+                </TabPanel>
+            <TabPanel>
+                <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+                <p>three!</p>
+            </TabPanel>
+        </TabPanels>
+    </Tabs>
       <h2>Recent</h2>
       <ul>
         <li>Liên quan đến Chương trình cấp phép việc làm</li>
