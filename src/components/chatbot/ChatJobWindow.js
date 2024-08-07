81bc5f34e3da04e24a68bbee2701136c8cc51026
Sophie1106 <yoonds714@nate.com>
2024-08-07 21:52:32 +0900

    job-pro매치

--- src/components/chatbot/ChatJobWindow.js
+++ src/components/chatbot/ChatJobWindow.js
@@ -26,27 +26,35 @@ function ChatJobWindow() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (input.trim()) {
-      const userMessage = { id: messages.length + 1, text: input, isUser: true };
-      setMessages([...messages, userMessage]);
-      setInput('');
-  
-      try {
-        // 백엔드로 메시지 전송 및 응답 처리
-        const response = await axios.post('http://localhost:8000/ask', { query: input });
-  
-        // 응답 데이터와 상태 코드 확인
-        console.log('Response:', response.data); // 전체 응답 데이터
-        console.log('Status:', response.status); // 상태 코드
-  
-        // 응답에서 output을 추출. 사용자 메시지와 봇 메시지를 각각 고유한 ID로 구분
-        const botMessage = { id: messages.length + 2, text: response.data.output, isUser: false };
-        setMessages([...messages, userMessage, botMessage]);
-      } catch (error) {
-        console.error('Error sending message:', error.response ? error.response.data : error.message);
-        setMessages([...messages, userMessage, { id: messages.length + 2, text: "Sorry, there was an error processing your request.", isUser: false }]);
-      }
+        const userMessage = { id: messages.length + 1, text: input, isUser: true };
+        setMessages([...messages, userMessage]);
+        setInput('');
+
+        try {
+            const formData = new FormData();
+            formData.append('query', input);
+
+            // 백엔드로 메시지 전송 및 응답 처리
+            const response = await axios.post('http://localhost:8000/search_jobs', formData, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+
+            // 응답 데이터와 상태 코드 확인
+            console.log('Response:', response.data); // 전체 응답 데이터
+            console.log('Status:', response.status); // 상태 코드
+
+            // 응답에서 output을 추출. 사용자 메시지와 봇 메시지를 각각 고유한 ID로 구분
+            const botMessage = { id: messages.length + 2, text: response.data.response, isUser: false };
+            setMessages([...messages, userMessage, botMessage]);
+        } catch (error) {
+            console.error('Error sending message:', error.response ? error.response.data : error.message);
+            setMessages([...messages, userMessage, { id: messages.length + 2, text: "Sorry, there was an error processing your request.", isUser: false }]);
+        }
     }
-  };
+};
+
   
   
 
